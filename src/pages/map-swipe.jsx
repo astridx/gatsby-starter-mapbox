79477fb9a5887fa1dcf5dea7b5bf02cc5c422b96
astridx <info@astrid-guenther.de>
2020-10-17 20:41:54 +0200

    fix

--- src/pages/map-swipe.jsx
+++ src/pages/map-swipe.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
+import React from 'react'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import styled from 'style'
 import Layout from 'components/Layout'
 import Swipemap from 'components/Swipemap'
-import swipeData from '../constants/swipe'
-import Compare from 'mapbox-gl-compare'
-import { Box, Flex } from 'components/Grid'
 
 const MapPage = () => {
   return (
